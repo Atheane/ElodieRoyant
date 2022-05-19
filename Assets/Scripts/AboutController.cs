2db964739b6a82a00c354a65fe206c76300b5316
Elodie Royant <eroyant@gmail.com>
2022-05-19 13:25:25 +0200

    feat(about) add links

---
+++ Assets/Scripts/AboutController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AboutController : MonoBehaviour
+{
+    public void OnClickMalt()
+    {
+        Application.OpenURL("https://www.malt.fr/profile/elodieroyant");
+    }
+    public void OnClickLinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/elodie-royant-472b00b");
+    }
+    public void OnClickButtonClose()
+    {
+        SceneManager.LoadScene("StartPage", LoadSceneMode.Single);
+    }
+}
